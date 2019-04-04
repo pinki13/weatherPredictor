18b68daa8993a6035642f8fd98bab8bd85b53f37
sanjika Kumari <ksanjika13@gmail.com>
2019-04-04 09:58:46 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,79 @@
+const WEATHER_SEARCH_URL = "https://api.openweathermap.org/data/2.5/weather?id=524901&APPID=d86b9843fdc4941e520f985922146256"
+//const FOURSQUARE_SEARCH_URL = "https://api.foursquare.com/v2/venues/explore?&client_id=FPRD2S2RFIB4QLBNBBHNAMLYOUF2AZSZ21ZK53QYASWCRJ1Z&client_secret=FEFA44EG0YDZ0XKA1UWX5ZWLZJLE30E2GYRLGB44PKE5KZ0E&v=20170915"
+
+//press on submit button and scroll to results
+function scrollPageTo(myTarget, topPadding) {
+    if (topPadding == undefined) {
+        topPadding = 0;
+    }
+    var moveTo = $(myTarget).offset().top - topPadding;
+    $('html, body').stop().animate({
+        scrollTop: moveTo
+    }, 200);
+}
+
+
+//retrieve data from OpenWeather API
+function getWeatherData() {
+    let city = $('.search-query').val();
+    $.ajax(WEATHER_SEARCH_URL, {
+        data: {
+            units: 'imperial',
+            q: city
+        },
+        dataType: 'jsonp',
+        type: 'GET',
+        success: function (data) {
+            let widget = displayWeather(data);
+            $('#weather-display').html(widget);
+            scrollPageTo('#weather-display', 15);
+        }
+    });
+}
+
+function displayWeather(data) {
+    return `
+    <div class="weather-results">
+        <h1><strong>Current Weather for ${data.name}</strong></h1>
+        <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
+        <p style="font-size:30px; margin-top:10px;">${data.weather[0].main}</p>
+        <p style="color:steelblue;" ">Description:</p><p"> ${data.weather[0].description}</p>
+        <p style="color:steelblue;">Temperature:</p><p> ${data.main.temp} &#8457; / ${(((data.main.temp)-32)*(5/9)).toFixed(2)} &#8451;</p>
+        <p style="color:steelblue;">Min. Temperature:</p><p> ${data.main.temp_min} &#8457; / ${(((data.main.temp_min)-32)*(5/9)).toFixed(2)} &#8451</p>
+        <p style="color:steelblue;">Max. Temperature:</p><p> ${data.main.temp_max} &#8457; / ${(((data.main.temp_max)-32)*(5/9)).toFixed(2)} &#8451</p>
+        <p style="color:steelblue;">Humidity:</p><p> ${data.main.humidity} &#37;</p>
+    </div>
+`;
+}
+
+
+
+
+function enterLocation() {
+    $('.category-button').click(function () {
+        $('button').removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $('.search-form').submit(function (event) {
+        event.preventDefault();
+        $('.navigation').removeClass("hide");
+        $('#weather-display').html("");
+        $('#foursquare-results').html("");
+        getWeatherData();
+    
+        $('button').removeClass("selected");
+    });
+}
+
+//autocomplete location name in form
+function activatePlacesSearch() {
+    let options = {
+        types: ['(regions)']
+    };
+    let input = document.getElementById('search-term');
+    let autocomplete = new google.maps.places.Autocomplete(input, options);
+}
+
+
+$(enterLocation);
